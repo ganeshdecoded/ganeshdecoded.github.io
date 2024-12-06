85f0cc35b04be296fdeeb77867849c24901da59d
gotech123 <gaminginfinity899@gmail.com>
2024-12-06 12:14:20 +0530

    4 changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2308465071"
+    serviceWorkerVersion: "2513009698"
   }
 });
